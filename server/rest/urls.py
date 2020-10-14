23c58b7ba1ae7a6d4ce3ece22de766272cf200ce
ka-r-ol <karol.lemanski@wp.pl>
2020-10-14 21:10:10 +0200

    REST API ready

--- server/rest/urls.py
+++ server/rest/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('expenses', views.ExpenseListCreateView.as_view()),
     path('categories', views.CategoryListCreateView.as_view()),
     path('me', views.Me.as_view()),
+    path('stats', views.Stats.as_view()),
     path('expense/new', views.ExpenseListCreateView.as_view()),
     path('category/new', views.CategoryListCreateView.as_view()),
     path('expense/<int:pk>', views.ExpenseRetriveUpdateDestroyView.as_view()),
--- server/rest/views.py
+++ server/rest/views.py
@@ -1,10 +1,10 @@
 from rest.models import Expense, Category
 from django.contrib.auth.models import User
-
 #from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 #from rest_framework.exceptions import ValidationError
 from rest_framework import permissions
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from rest.serializers import CategorySerializer, ExpenseSerializer, UserSerializer
 from rest.filters import ExpenseFilter
@@ -51,3 +51,27 @@ class CategoryRetriveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all().order_by('name')
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class Stats(ListAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ExpenseFilter
+
+    def get_queryset(self, *args, **kwargs):
+        return Expense.objects.all().filter(owner=self.request.user).order_by('-date', '-created')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        data = dict()
+        for q in queryset:
+            if q.category.name in data:
+                data[q.category.name]['number'] += 1
+                data[q.category.name]['cost'] += q.cost
+            else:
+                data[q.category.name] = dict()
+                data[q.category.name]['number'] = 1
+                data[q.category.name]['cost'] = q.cost
+        return Response(data)
